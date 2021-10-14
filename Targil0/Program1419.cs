796c25e2b18a24b2bd14f397128784c3add64ef0
yechezkel chen <yl9753@gmail.com>
2021-10-14 17:01:52 +0300

    I add output and input to program

---
+++ Targil0/Program1419.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome1419();
+            Welcome0066();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome0066();
+
+        private static void Welcome1419()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
