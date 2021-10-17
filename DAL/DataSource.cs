2d2467955fe4f904a71bcd7b4f93a25528eaf4b6
yechezkel chen <yl9753@gmail.com>
2021-10-17 12:51:57 +0300

    I fix mistakes in code

--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using IDAL.DO;
+using DalObjects;
 
 namespace DalObjects
 {
     internal class DataSource
     {
-        IDAL.DO.Drone[] Drones = new IDAL.DO.Drone[10];
-        IDAL.DO.Station[] Stations = new IDAL.DO.Station[5];
-        IDAL.DO.Customer[] Customers = new IDAL.DO.Customer[100];
-        IDAL.DO.Parcel[] Parcels = new IDAL.DO.Parcel[1000];
+        Drone[] Drones = new Drone[10];
+        Station[] Stations = new Station[5];
+        Customer[] Customers = new Customer[100];
+        Parcel[] Parcels = new Parcel[1000];
 
         internal class Config
         {
@@ -20,32 +22,40 @@ namespace DalObjects
 
         public static void Initialize()
         {
-            IDAL.DO.Drone[] DataSource.Drones[0] = { 1111, a, "Light weight", "Available status", 100};
-            IDAL.DO.Drone[] DataSource.Drones[1] = { 2222, b, "Medium weight", "Available status", 90};
-            IDAL.DO.Drone[] DataSource.Drones[2] = { 3333, c, "Heavy weight", "Available status", 80};
-            IDAL.DO.Drone[] DataSource.Drones[3] = { 4444, d, "Light weight", "Delivery status", 70};
-            IDAL.DO.Drone[] DataSource.Drones[4] = { 5555, e, "Medium weight", "Delivery status", 60};
+            Random rand = new Random(DateTime.Now.Millisecond);
+            Drones[0] = new Drone { Id = rand.Next(100000000, 999999999), Model = "a", MaxWeight = WeightCategories.Light, Status = DroneStatuses.Available, Battry = 100 };
+            Drones[1] = new Drone { Id = rand.Next(100000000, 999999999), Model = "b", MaxWeight = WeightCategories.Medium, Status = DroneStatuses.Available, Battry = 90 };
+            Drones[2] = new Drone { Id = rand.Next(100000000, 999999999), Model = "c", MaxWeight = WeightCategories.Heavy, Status = DroneStatuses.Available, Battry = 80 };
+            Drones[3] = new Drone { Id = rand.Next(100000000, 999999999), Model = "d", MaxWeight = WeightCategories.Light, Status = DroneStatuses.Delivery, Battry = 70 };
+            Drones[4] = new Drone { Id = rand.Next(100000000, 999999999), Model = "e", MaxWeight = WeightCategories.Medium, Status = DroneStatuses.Delivery, Battry = 60 };
             Config.VacantIndexD = 5;
 
             for (int i = 0; i < 2; i++)
-                IDAL.DO.Station[] DataSource.Stations[i] = { i, i + i, rand.Next(), rand.Next(), i + i + i}
+                Stations[i] = new Station { Id = rand.Next(100000000, 999999999), Name = i + i, Longitude = rand.Next(10, 1000), Lattitued = rand.Next(10, 1000), ChargeSlots = i + i + i };
             Config.VacantIndexS = 2;
 
-            for (int i = 0, char ch = 'a', string ph = "0525552240"; i < 10; i++, ch++, ph[9] = i)
-                IDAL.DO.Customer[] DataSource.Customers[i] = { i, ch, ph, rand.Next(), rand.Next()}
+            Customers[0] = new Customer { Id = rand.Next(100000000, 999999999), Name = "a", Phone = "0541234981", Longitude = rand.Next(10, 1000), Lattitued = rand.Next(10, 1000) };
+            Customers[1] = new Customer { Id = rand.Next(100000000, 999999999), Name = "b", Phone = "0541234982", Longitude = rand.Next(10, 1000), Lattitued = rand.Next(10, 1000) };
+            Customers[2] = new Customer { Id = rand.Next(100000000, 999999999), Name = "c", Phone = "0541234983", Longitude = rand.Next(10, 1000), Lattitued = rand.Next(10, 1000) };
+            Customers[3] = new Customer { Id = rand.Next(100000000, 999999999), Name = "d", Phone = "0541234984", Longitude = rand.Next(10, 1000), Lattitued = rand.Next(10, 1000) };
+            Customers[4] = new Customer { Id = rand.Next(100000000, 999999999), Name = "e", Phone = "0541234985", Longitude = rand.Next(10, 1000), Lattitued = rand.Next(10, 1000) };
+            Customers[5] = new Customer { Id = rand.Next(100000000, 999999999), Name = "f", Phone = "0541234986", Longitude = rand.Next(10, 1000), Lattitued = rand.Next(10, 1000) };
+            Customers[6] = new Customer { Id = rand.Next(100000000, 999999999), Name = "g", Phone = "0541234987", Longitude = rand.Next(10, 1000), Lattitued = rand.Next(10, 1000) };
+            Customers[7] = new Customer { Id = rand.Next(100000000, 999999999), Name = "h", Phone = "0541234988", Longitude = rand.Next(10, 1000), Lattitued = rand.Next(10, 1000) };
+            Customers[8] = new Customer { Id = rand.Next(100000000, 999999999), Name = "i", Phone = "0541234989", Longitude = rand.Next(10, 1000), Lattitued = rand.Next(10, 1000) };
+            Customers[9] = new Customer { Id = rand.Next(100000000, 999999999), Name = "g", Phone = "0541234990", Longitude = rand.Next(10, 1000), Lattitued = rand.Next(10, 1000) };
             Config.VacantIndexC = 10;
 
-            static Random rand = new Random(DateTime.Now.Millisecond);
-            IDAL.DO.Parcel[] DataSource.Parcels[0] = { 1, 11, 111, "Light weight", "Normal Priority", 1111, rand.Next(), rand.Next(), rand.Next(), rand.Next()};
-            IDAL.DO.Parcel[] DataSource.Parcels[1] = { 2, 22, 222, "Medium weight" ,"Normal Priority", 2222,rand.Next(),rand.Next(),rand.Next(),rand.Next()};
-            IDAL.DO.Parcel[] DataSource.Parcels[2] = { 3, 33, 333, "Heavy weight", "Normal Priority", 3333, rand.Next(), rand.Next(), rand.Next(), rand.Next()};
-            IDAL.DO.Parcel[] DataSource.Parcels[3] = { 4 , 44, 444, "Light weight" ,"Fast Priority", 4444,rand.Next(),rand.Next(),rand.Next(),rand.Next()};
-            IDAL.DO.Parcel[] DataSource.Parcels[4] = { 5, 55, 555, "Medium weight", "Fast Priority", 5555, rand.Next(), rand.Next(), rand.Next(), rand.Next()};
-            IDAL.DO.Parcel[] DataSource.Parcels[5] = { 6, 66, 666, "Heavy weight", "Fast Priority", 6666, rand.Next(), rand.Next(), rand.Next(), rand.Next()};
-            IDAL.DO.Parcel[] DataSource.Parcels[6] = { 7, 77, 777, "Light weight", "Normal Priority", 7777, rand.Next(), rand.Next(), rand.Next(), rand.Next()};
-            IDAL.DO.Parcel[] DataSource.Parcels[7] ={ 8 , 88, 888, "Heavy weight" ,"Emergency Priority", 8888,rand.Next(),rand.Next(),rand.Next(),rand.Next()};
-            IDAL.DO.Parcel[] DataSource.Parcels[8] ={ 9 , 99, 999, "Medium weight" ,"Emergency Priority", 9999,rand.Next(),rand.Next(),rand.Next(),rand.Next()};
-            IDAL.DO.Parcel[] DataSource.Parcels[9] ={ 10 , 1010, 1010, "Light weight" ,"Emergency Priority", 4444,rand.Next(),rand.Next(),rand.Next(),rand.Next()};
+            Parcels[0] = new Parcel { Id = rand.Next(100000000, 999999999), SenderId = rand.Next(100000000, 999999999), TargetId = rand.Next(100000000, 999999999), Weight = WeightCategories.Light, Priority = Priorities.Normal, DroneId = Drones[0].Id, Requested = DateTime.Now, Scheduled = DateTime.Now, PickedUp = DateTime.Now, Delivered = DateTime.Now };
+            Parcels[1] = new Parcel { Id = rand.Next(100000000, 999999999), SenderId = rand.Next(100000000, 999999999), TargetId = rand.Next(100000000, 999999999), Weight = WeightCategories.Medium, Priority = Priorities.Normal, DroneId = Drones[1].Id, Requested = DateTime.Now, Scheduled = DateTime.Now, PickedUp = DateTime.Now, Delivered = DateTime.Now };
+            Parcels[2] = new Parcel { Id = rand.Next(100000000, 999999999), SenderId = rand.Next(100000000, 999999999), TargetId = rand.Next(100000000, 999999999), Weight = WeightCategories.Heavy, Priority = Priorities.Normal, DroneId = Drones[2].Id, Requested = DateTime.Now, Scheduled = DateTime.Now, PickedUp = DateTime.Now, Delivered = DateTime.Now };
+            Parcels[3] = new Parcel { Id = rand.Next(100000000, 999999999), SenderId = rand.Next(100000000, 999999999), TargetId = rand.Next(100000000, 999999999), Weight = WeightCategories.Light, Priority = Priorities.Fast, DroneId = Drones[3].Id, Requested = DateTime.Now, Scheduled = DateTime.Now, PickedUp = DateTime.Now, Delivered = DateTime.Now };
+            Parcels[4] = new Parcel { Id = rand.Next(100000000, 999999999), SenderId = rand.Next(100000000, 999999999), TargetId = rand.Next(100000000, 999999999), Weight = WeightCategories.Medium, Priority = Priorities.Fast, DroneId = Drones[4].Id, Requested = DateTime.Now, Scheduled = DateTime.Now, PickedUp = DateTime.Now, Delivered = DateTime.Now };
+            Parcels[5] = new Parcel { Id = rand.Next(100000000, 999999999), SenderId = rand.Next(100000000, 999999999), TargetId = rand.Next(100000000, 999999999), Weight = WeightCategories.Heavy, Priority = Priorities.Fast, DroneId = Drones[0].Id, Requested = DateTime.Now, Scheduled = DateTime.Now, PickedUp = DateTime.Now, Delivered = DateTime.Now };
+            Parcels[6] = new Parcel { Id = rand.Next(100000000, 999999999), SenderId = rand.Next(100000000, 999999999), TargetId = rand.Next(100000000, 999999999), Weight = WeightCategories.Light, Priority = Priorities.Normal, DroneId = Drones[1].Id, Requested = DateTime.Now, Scheduled = DateTime.Now, PickedUp = DateTime.Now, Delivered = DateTime.Now };
+            Parcels[7] = new Parcel { Id = rand.Next(100000000, 999999999), SenderId = rand.Next(100000000, 999999999), TargetId = rand.Next(100000000, 999999999), Weight = WeightCategories.Heavy, Priority = Priorities.Emergency, DroneId = Drones[2].Id, Requested = DateTime.Now, Scheduled = DateTime.Now, PickedUp = DateTime.Now, Delivered = DateTime.Now };
+            Parcels[8] = new Parcel { Id = rand.Next(100000000, 999999999), SenderId = rand.Next(100000000, 999999999), TargetId = rand.Next(100000000, 999999999), Weight = WeightCategories.Medium, Priority = Priorities.Emergency, DroneId = Drones[3].Id, Requested = DateTime.Now, Scheduled = DateTime.Now, PickedUp = DateTime.Now, Delivered = DateTime.Now };
+            Parcels[9] = new Parcel { Id = rand.Next(100000000, 999999999), SenderId = rand.Next(100000000, 999999999), TargetId = rand.Next(100000000, 999999999), Weight = WeightCategories.Light, Priority = Priorities.Emergency, DroneId = Drones[4].Id, Requested = DateTime.Now, Scheduled = DateTime.Now, PickedUp = DateTime.Now, Delivered = DateTime.Now };
             Config.VacantIndexP = 10;
             Config.ParcelsId = 11;
         }
