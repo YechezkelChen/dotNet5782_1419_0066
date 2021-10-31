dfe694c8821f32bd3dc43669f89f98b75363bcf9
yechezkel chen <yl9753@gmail.com>
2021-10-31 10:36:05 +0200

    add IDal

---
+++ DAL/IDal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IDAL.DO;
+
+namespace IDal
+{
+    class IDal
+    {
+        public static void AddStation(Station newStation);
+
+    }
+}
