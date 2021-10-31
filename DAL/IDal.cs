8c2f016b5894453430cffa0cafdb132ae5be0033
yechezkel chen <yl9753@gmail.com>
2021-10-31 17:08:34 +0200

    change

--- DAL/IDal.cs
+++ DAL/IDal.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IDAL;
 using IDAL.DO;
 
-namespace IDal
+namespace IDAL
 {
     interface IDal
     { 
