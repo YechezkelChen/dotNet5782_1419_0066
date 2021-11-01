80fda63191d5e1bae78d6131b09967789c8f2a09
davidnoam4 <69966679+davidnoam4@users.noreply.github.com>
2021-11-01 09:39:54 +0200

    j

--- BL/Station.cs
+++ BL/Station.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IDAL.DO;
 
 namespace IBL
 {
@@ -10,7 +11,11 @@ namespace IBL
     {
         public class Station
         {
-
+            public int id { get; set; }
+            public int name { get; set; }
+            public Location location { get; set; }
+            public int chargeSlots { get; set; }
+            public List<DroneCharge> inCharges { get; set; }
         }
     }
 }
