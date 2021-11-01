ce096774f9b7ec882eaf923243447a8294a13175
davidnoam4 <69966679+davidnoam4@users.noreply.github.com>
2021-11-01 13:13:57 +0200

    צריך לראות איך אפשר להעתיק את השדות הרלוונטים לרשימה של רחפנים מסוג בו מרחפנים מסוג דו ואז נוכל לבצע השוואה נורמלית ולעדכן את הסטטוס של אותו רחפן ששויך כבר לחבילה מסויימת אבל לא סיפק אותה

--- BL/BL.cs
+++ BL/BL.cs
@@ -11,6 +11,8 @@ namespace BL
     public partial class BL : IBL
     {
         public IEnumerable<IDAL.DO.Drone> listDrones = new List<IDAL.DO.Drone>();
+        public IEnumerable<IDAL.DO.Parcel> ListParcels = new List<IDAL.DO.Parcel>();
+        public IEnumerable<IBL.BO.Drone> listBoDrones = new List<IBL.BO.Drone>();
 
         public BL()
         {
@@ -24,6 +26,22 @@ namespace BL
             double chargingRateOfDrone = powerConsumption[4]; //Percent per hour
 
             listDrones = dal.GetDrones();
+            ListParcels = dal.GetParcels();
+            //צריך לראות איך אפשר להעתיק את השדות הרלוונטים לרשימה של רחפנים מסוג בו מרחפנים מסוג דו ואז נוכל לבצע השוואה נורמלית ולעדכן את הסטטוס בהתתאם
+            foreach (IDAL.DO.Drone elementDrone in listDrones)
+            {
+                foreach (IDAL.DO.Parcel elmentParcel in ListParcels)
+                {
+                    if (elmentParcel.pickedUp == DateTime.MinValue && elmentParcel.droneId == elementDrone.id)//ther are parcel that not pickup but drone that connected to the parcel
+                        
+                }
+
+            }
+            
+            
+
+
+
 
         }
     }
