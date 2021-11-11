495fa8f8763a8378dbb5bc98894309964af284ab
davidnoam4 <69966679+davidnoam4@users.noreply.github.com>
2021-11-11 13:57:10 +0200

    SS

--- BL/BLParcel.cs
+++ BL/BLParcel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using IBL.BO;
@@ -135,22 +137,93 @@ namespace IBL
 
         public void ConnectParcelToDrone(int droneId)
         {
-            IDAL.DO.Drone connectDrone = new IDAL.DO.Drone();
             try
             {
-                connectDrone = dal.GetDrone(droneId);
+                dal.GetDrone(droneId);
             }
             catch (DroneException e)
             {
                 throw new DroneException(""+e);
             }
 
-            drone
-            foreach (var elementListDrone in ListDrones)
+            Drone connectDrone = GetDrone(droneId);
+            if (connectDrone.Status != DroneStatuses.Available)
+                throw new DataException("The drone is not available:\n ");
+
+            foreach (var elementParcelsNoDrone in GetParcelsNoDrones())
             {
-                if (droneId == elementListDrone.Id) 
+                if (elementParcelsNoDrone.Priority == Priorities.Emergency)
+                {
+                    bool flag = new bool();
+                    flag = false;
+
+                    if (connectDrone.Weight == WeightCategories.Heavy)
+                        flag = true;
+
+                    if (connectDrone.Weight == WeightCategories.Medium &&
+                        (elementParcelsNoDrone.Weight == WeightCategories.Medium ||
+                         elementParcelsNoDrone.Weight == WeightCategories.Light))
+                        flag = true;
+
+                    if (connectDrone.Weight == WeightCategories.Light &&
+                        elementParcelsNoDrone.Weight == WeightCategories.Light)
+                        flag = true;
+
+                    if (!flag)
+                        throw new DataException(
+                            "The maximum weight that the drone can carry is less than the weight of the parcel :(\n ");
+                    Customer senderCustomer = GetCustomer(elementParcelsNoDrone.SenderId);
+                    IDAL.DO.Customer targetCustomer = dal.GetCustomer(elementParcelsNoDrone.TargetId);
+
+                    Location targetLocation = new Location()
+                        {Longitude = targetCustomer.Longitude, Latitude = targetCustomer.Latitude};
+
+                    double distanceDelivery = Distance(connectDrone.Location, senderCustomer.Location);
+                    double batteryDelivery = distanceDelivery * dAvailable;
+
+                    distanceDelivery = Distance(senderCustomer.Location, targetLocation); // the distance between the drone and the target
+                    if (elementParcelsNoDrone.Weight == WeightCategories.Heavy)
+                        batteryDelivery += distanceDelivery * dHeavyW;
+                    if (elementParcelsNoDrone.Weight == WeightCategories.Medium)
+                        batteryDelivery += distanceDelivery * dMediumW;
+                    if (elementParcelsNoDrone.Weight == WeightCategories.Light)
+                        batteryDelivery += distanceDelivery * dLightW;
+
+                    distanceDelivery = Distance(targetLocation, NearStationToCustomer(targetCustomer).Location);
+                    batteryDelivery += distanceDelivery * dAvailable;
+
+                    if (connectDrone.Battery < batteryDelivery)
+                        throw new DroneException("ERROR: The drone not have much battery to send the parcel!!!\n");
+                    connectDrone.Status = DroneStatuses.Delivery;
+                    for (int i = 0; i < ListDrones.Count(); i++)
+                    {
+                        if (ListDrones[i].Id == connectDrone.Id)
+                        {
+                            DroneToList updateDrone = ListDrones[i];
+                            updateDrone.Status = DroneStatuses.Delivery;
+                            ListDrones[i] = updateDrone;
+                        }
+                    }
+
+
+                    IDAL.DO.Parcel updateParcel = dal.GetParcel(elementParcelsNoDrone.Id);
+                    updateParcel.DroneId = connectDrone.Id;
+                    dal.UpdateParcel(updateParcel);
+
+
+                }
+                  
+
+                }
+                    
+                    
+
+
+
 
             }
+
+
 
         }
     }
