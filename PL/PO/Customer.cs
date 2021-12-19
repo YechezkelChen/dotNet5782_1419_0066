767990c54a049f2dcc46be5b785dbe2874f3590a
yechezkel chen <yl9753@gmail.com>
2021-12-19 20:05:20 +0200

    change

---
+++ PL/PO/Customer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PO
+{
+    public class Customer : INotifyPropertyChanged
+    {
+        private int id;
+        private string Name;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; OnPropertyChanged("Id"); }
+        }
+        public string Name
+        {
+            get { return id; }
+            set
+            {
+                id = value; OnPropertyChanged("Id");
+            }
+        }
+        public string Phone { get; set; }
+        public Location Location { get; set; }
+        public IEnumerable<ParcelInCustomer> FromTheCustomerList { get; set; }
+        public IEnumerable<ParcelInCustomer> ToTheCustomerList { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
