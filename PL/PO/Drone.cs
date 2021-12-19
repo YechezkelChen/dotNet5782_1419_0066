dcdb07625751181df94444f7770f914d5ff98fcc
davidnoam4 <69966679+davidnoam4@users.noreply.github.com>
2021-12-19 20:35:55 +0200

    PO

---
+++ PL/PO/Drone.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PO 
+{
+    public class Drone : INotifyPropertyChanged
+    {
+        public int Id
+        {
+            get { return Id; }
+            set { Id = value; OnPropertyChanged("Id"); }
+        }
+
+        public String Model
+        {
+            get { return Model; }
+            set { Model = value; OnPropertyChanged("Model"); }
+        }
+
+        public WeightCategories Weight
+        {
+            get { return Weight; }
+            set { Weight = value; OnPropertyChanged("Weight"); }
+        }
+
+        public double Battery
+        {
+            get { return Battery;}
+            set { Battery = value; OnPropertyChanged("Battery"); }
+        }
+
+        public DroneStatuses Status
+        {
+            get { return  Status; }
+            set { Status = value; OnPropertyChanged("Status"); }
+        }
+
+        public ParcelByTransfer ParcelByTransfer
+        {
+            get { return  ParcelByTransfer; }
+            set { ParcelByTransfer = value; OnPropertyChanged("Status"); }
+        }
+
+        public Location Location
+        {
+            get {return  Location; }
+            set { Location = value; OnPropertyChanged("Status"); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ PL/PO/Enums.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+
+
+namespace PO
+{
+    public enum WeightCategories
+    {
+        [Description("Light Weight")]
+        Light,
+        [Description("Medium Weight")]
+        Medium,
+        [Description("Heavy Weight")]
+        Heavy
+    }
+
+    public enum DroneStatuses
+    {
+        [Description("Available Status")]
+        Available,
+        [Description("Maintenance Status")]
+        Maintenance,
+        [Description("Delivery Status")]
+        Delivery
+    }
+
+    public enum Priorities
+    {
+        [Description("Normal Priority")]
+        Normal,
+        [Description("Fast Priority")]
+        Fast,
+        [Description("Emergency Priority")]
+        Emergency
+    }
+
+    public enum ParcelStatuses
+    {
+        [Description("Requested Status")]
+        Requested,
+        [Description("Scheduled Status")]
+        Scheduled,
+        [Description("PickedUp Status")]
+        PickedUp,
+        [Description("Delivered Status")]
+        Delivered
+    }
+}
