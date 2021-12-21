f4e0e54bead4961fcc3fea8eab73dccaf7f4c566
yechezkel chen <yl9753@gmail.com>
2021-12-21 19:38:35 +0200

    change

--- BL/BL/BLParcel.cs
+++ BL/BL/BLParcel.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Net.Sockets;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 using BO;
 
 
--- DalFacade/DO/Station.cs
+++ DalFacade/DO/Station.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace DO
+﻿namespace DO
 {
     public struct Station
     {
