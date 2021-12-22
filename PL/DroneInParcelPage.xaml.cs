e36d255998f2bb75cf9fce1398511a0d20ac1861
davidnoam4 <69966679+davidnoam4@users.noreply.github.com>
2021-12-22 19:10:28 +0200

    d

--- PL/DroneInParcelPage.xaml.cs
+++ PL/DroneInParcelPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using BO;
 
 namespace PL
 {
@@ -20,9 +21,17 @@ namespace PL
     /// </summary>
     public partial class DroneInParcelPage : Page
     {
+        private BlApi.IBL bl = BlApi.BlFactory.GetBl();
+        private Drone drone;
         public DroneInParcelPage()
         {
             InitializeComponent();
         }
+
+        private void ClosePageButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Content = "";
+        }
+
     }
 }
