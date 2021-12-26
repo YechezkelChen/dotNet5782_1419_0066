74d82acf57fc491e45ad3015200abe0f097f8260
yechezkel chen <yl9753@gmail.com>
2021-12-26 12:26:27 +0200

    change

--- PL/ParcelInDronePage.xaml.cs
+++ PL/ParcelInDronePage.xaml.cs
@@ -16,6 +16,7 @@ namespace PL
             InitializeComponent();
             listWindow = window;
             this.drone = drone;
+            this.DataContext = drone.ParcelByTransfer;
             ShowParcelInDrone();
         }
 
@@ -26,30 +27,15 @@ namespace PL
 
         void ShowParcelInDrone()
         {
-            if (drone.ParcelByTransfer is null)
-            {
-                IdParcelTextBox.Text = "";
-                StatusParcelTextBox.Text = "";
-                PriorityParcelTextBox.Text = "";
-                WeightParcelTextBox.Text = "";
-                SenderInParcelTextBox.Text = "";
-                PickUpLocationParcelTextBox.Text = "";
-                ReceiverInParcelTextBox.Text = "";
-                TargetLocationParcelTextBox.Text = "";
-                DistanceOfTransferTextBox.Text = "";
-            }
-            else
-            {
-                IdParcelTextBox.Text = drone.ParcelByTransfer.Id.ToString();
-                StatusParcelTextBox.Text = drone.ParcelByTransfer.OnTheWay.ToString();
-                PriorityParcelTextBox.Text = drone.ParcelByTransfer.Priority.ToString();
-                WeightParcelTextBox.Text = drone.ParcelByTransfer.Weight.ToString();
-                SenderInParcelTextBox.Text = drone.ParcelByTransfer.Sender.ToString();
-                PickUpLocationParcelTextBox.Text = drone.ParcelByTransfer.PickUpLocation.ToString();
-                ReceiverInParcelTextBox.Text = drone.ParcelByTransfer.Target.ToString();
-                TargetLocationParcelTextBox.Text = drone.ParcelByTransfer.TargetLocation.ToString();
-                DistanceOfTransferTextBox.Text = drone.ParcelByTransfer.DistanceOfTransfer.ToString();
-            }
+            //IdParcelTextBox.Text = drone.ParcelByTransfer.Id.ToString();
+            //StatusParcelTextBox.Text = drone.ParcelByTransfer.OnTheWay.ToString();
+            PriorityParcelTextBox.Text = drone.ParcelByTransfer.Priority.ToString();
+            WeightParcelTextBox.Text = drone.ParcelByTransfer.Weight.ToString();
+            SenderInParcelTextBox.Text = drone.ParcelByTransfer.Sender.ToString();
+            PickUpLocationParcelTextBox.Text = drone.ParcelByTransfer.PickUpLocation.ToString();
+            ReceiverInParcelTextBox.Text = drone.ParcelByTransfer.Target.ToString();
+            TargetLocationParcelTextBox.Text = drone.ParcelByTransfer.TargetLocation.ToString();
+            DistanceOfTransferTextBox.Text = drone.ParcelByTransfer.DistanceOfTransfer.ToString();
         }
     }
 }
