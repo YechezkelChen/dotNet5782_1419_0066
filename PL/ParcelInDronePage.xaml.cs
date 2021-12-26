b7df6deccbd3e7b2b0f958ffa154ca490b4cb654
yechezkel chen <yl9753@gmail.com>
2021-12-26 12:32:23 +0200

    change

--- PL/ParcelInDronePage.xaml.cs
+++ PL/ParcelInDronePage.xaml.cs
@@ -17,7 +17,7 @@ namespace PL
             listWindow = window;
             this.drone = drone;
             DataParcelGrid.DataContext = drone.ParcelByTransfer;
-            ShowParcelInDrone();
+            //ShowParcelInDrone();
         }
 
         private void ClosePageButton_Click(object sender, RoutedEventArgs e)
@@ -25,17 +25,17 @@ namespace PL
             //listWindow.ShowData.Content = new DronePage(listWindow, drone);
         }
 
-        void ShowParcelInDrone()
-        {
-            //IdParcelTextBox.Text = drone.ParcelByTransfer.Id.ToString();
-            //StatusParcelTextBox.Text = drone.ParcelByTransfer.OnTheWay.ToString();
-            //PriorityParcelTextBox.Text = drone.ParcelByTransfer.Priority.ToString();
-            //WeightParcelTextBox.Text = drone.ParcelByTransfer.Weight.ToString();
-            //SenderInParcelTextBox.Text = drone.ParcelByTransfer.Sender.ToString();
-            //PickUpLocationParcelTextBox.Text = drone.ParcelByTransfer.PickUpLocation.ToString();
-            //ReceiverInParcelTextBox.Text = drone.ParcelByTransfer.Target.ToString();
-            //TargetLocationParcelTextBox.Text = drone.ParcelByTransfer.TargetLocation.ToString();
-            //DistanceOfTransferTextBox.Text = drone.ParcelByTransfer.DistanceOfTransfer.ToString();
-        }
+        //void ShowParcelInDrone()
+        //{
+        //    //IdParcelTextBox.Text = drone.ParcelByTransfer.Id.ToString();
+        //    //StatusParcelTextBox.Text = drone.ParcelByTransfer.OnTheWay.ToString();
+        //    //PriorityParcelTextBox.Text = drone.ParcelByTransfer.Priority.ToString();
+        //    //WeightParcelTextBox.Text = drone.ParcelByTransfer.Weight.ToString();
+        //    //SenderInParcelTextBox.Text = drone.ParcelByTransfer.Sender.ToString();
+        //    //PickUpLocationParcelTextBox.Text = drone.ParcelByTransfer.PickUpLocation.ToString();
+        //    //ReceiverInParcelTextBox.Text = drone.ParcelByTransfer.Target.ToString();
+        //    //TargetLocationParcelTextBox.Text = drone.ParcelByTransfer.TargetLocation.ToString();
+        //    //DistanceOfTransferTextBox.Text = drone.ParcelByTransfer.DistanceOfTransfer.ToString();
+        //}
     }
 }
