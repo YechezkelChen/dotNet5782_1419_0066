2e9359d0ba4586034d710bf6ed1b82120a59d8db
yechezkel chen <yl9753@gmail.com>
2021-12-26 12:29:32 +0200

    change

--- PL/ParcelInDronePage.xaml.cs
+++ PL/ParcelInDronePage.xaml.cs
@@ -31,8 +31,8 @@ namespace PL
             //StatusParcelTextBox.Text = drone.ParcelByTransfer.OnTheWay.ToString();
             //PriorityParcelTextBox.Text = drone.ParcelByTransfer.Priority.ToString();
             //WeightParcelTextBox.Text = drone.ParcelByTransfer.Weight.ToString();
-            SenderInParcelTextBox.Text = drone.ParcelByTransfer.Sender.ToString();
-            PickUpLocationParcelTextBox.Text = drone.ParcelByTransfer.PickUpLocation.ToString();
+            //SenderInParcelTextBox.Text = drone.ParcelByTransfer.Sender.ToString();
+            //PickUpLocationParcelTextBox.Text = drone.ParcelByTransfer.PickUpLocation.ToString();
             ReceiverInParcelTextBox.Text = drone.ParcelByTransfer.Target.ToString();
             TargetLocationParcelTextBox.Text = drone.ParcelByTransfer.TargetLocation.ToString();
             DistanceOfTransferTextBox.Text = drone.ParcelByTransfer.DistanceOfTransfer.ToString();
