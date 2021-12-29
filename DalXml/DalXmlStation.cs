0442e69398e4fcc1aa773e4354971c23e3076149
davidnoam4 <69966679+davidnoam4@users.noreply.github.com>
2021-12-29 18:43:19 +0200

    station and customer Xml

--- DalXml/DalXmlStation.cs
+++ DalXml/DalXmlStation.cs
@@ -3,30 +3,108 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using DalXml;
 using DO;
 
 namespace Dal
 {
     partial class DalXml : DalApi.IDal
     {
+        /// <summary>
+        /// add a station to the stations list
+        /// </summary>
+        /// <param Name="newStation"></the new station the user whants to add to the station's list>
         public void AddStation(Station newStation)
         {
-            throw new NotImplementedException();
+            XElement stations = XMLTools.LoadListFromXmlElement(stationsPath);
+
+            var addStation = (from s in stations.Elements()
+                               where Convert.ToInt32(s.Element("Id").Value) == newStation.Id
+                               select s).FirstOrDefault();
+
+            if (addStation is null)
+                throw new IdNotFoundException("ERROR: the station is not found!\n");
+            if (addStation.Element("Deleted").Value == "true")
+                throw new IdNotFoundException("ERROR: the station is deleted!\n");
+
+
+            XElement id = new XElement("Id", newStation.Id);
+            XElement name = new XElement("Name", newStation.Name);
+            XElement longitude = new XElement("Longitude", newStation.Longitude);
+            XElement latitude = new XElement("Latitude", newStation.Latitude);
+            XElement availableChargeSlots = new XElement("AvailableChargeSlots", newStation.AvailableChargeSlots);
+            XElement deleted = new XElement("Deleted", newStation.Deleted);
+
+            stations.Add(new XElement("Station", id, name, longitude, latitude, availableChargeSlots , deleted));
+
+            XMLTools.SaveListToXmlElement(stations, stationsPath);
         }
 
+        /// <summary>
+        /// return the specific station the user ask for
+        /// </summary>
+        /// <param Name="stationId"></the Id of the station the user ask for>
+        /// <returns></returns>
         public Station GetStation(int stationId)
         {
-            throw new NotImplementedException();
+            Station getStation = new Station();
+            XElement stations = XMLTools.LoadListFromXmlElement(stationsPath);
+            try
+            {
+                getStation = (from station in stations.Elements()
+                               where Convert.ToInt32(station.Element("Id").Value) == stationId
+                              select new Station()
+                               {
+                                   Id = Convert.ToInt32(station.Element("Id").Value),
+                                   Name = station.Element("Name").Value,
+                                   Longitude = Convert.ToDouble(station.Element("Longitude").Value),
+                                   Latitude = Convert.ToDouble(station.Element("Latitude").Value),
+                                   AvailableChargeSlots = Convert.ToInt32(station.Element("AvailableChargeSlots").Value),
+                                   Deleted = Convert.ToBoolean(station.Element("Deleted").Value)
+                               }).FirstOrDefault();
+            }
+            catch
+            {
+                throw new IdNotFoundException("ERROR: the station is not found.");
+            }
+
+            if (getStation.Deleted == true)
+                throw new IdExistException("ERROR: the station was exist");
+
+            return getStation;
         }
 
+        /// <summary>
+        /// return all the station's list
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<Station> GetStations(Predicate<Station> stationPredicate)
         {
-            throw new NotImplementedException();
+            var stationsXml = XMLTools.LoadListFromXmlSerializer<Station>(stationsPath);
+            IEnumerable<Station> stations = stationsXml.Where(station => stationPredicate(station));
+            return stations;
         }
 
-        public void UpdateStation(Station station)
+        /// <summary>
+        /// update the specific station the user ask for
+        /// </summary>
+        /// <param name="updateStation"></param>
+        public void UpdateStation(Station updateStation)
         {
-            throw new NotImplementedException();
+            XElement stations = XMLTools.LoadListFromXmlElement(stationsPath);
+
+            XElement station = (from s in stations.Elements()
+                                 where Convert.ToInt32(s.Element("Id").Value) == updateStation.Id
+                                 select s).FirstOrDefault();
+
+            station.Element("Id").Value = updateStation.Id.ToString();
+            station.Element("Name").Value = updateStation.Name;
+            station.Element("Longitude").Value = updateStation.Longitude.ToString();
+            station.Element("Latitude").Value = updateStation.Latitude.ToString();
+            station.Element("AvailableChargeSlots").Value = updateStation.AvailableChargeSlots.ToString();
+            station.Element("Deleted").Value = updateStation.Deleted.ToString();
+            XMLTools.SaveListToXmlElement(stations, stationsPath);
         }
     }
 }
